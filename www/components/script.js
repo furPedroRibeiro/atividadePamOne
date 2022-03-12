d763202bc392b06f460130c6bb28175d45b2b10a
furPedroRibeiro <pedrocodejob@gmail.com>
2022-03-12 23:56:43 +0900

    Project's Initial Commit

--- www/components/script.js
+++ www/components/script.js
@@ -4,18 +4,19 @@
         let lado2=document.getElementById("lado2").value;
         let lado3=document.getElementById("lado3").value;
 
-        if(lado1>(lado2+lado3) && lado2>(lado1+lado3) && lado3>(lado1+lado2)){
-            alert("NÃO É TRIÂNGULO");
+
+        if(lado1>(lado2+lado3) || lado2>(lado1+lado3) || lado3>(lado1+lado2)){
+            document.getElementById("textarea").value="Não é triângulo";
         }
         else{
             if(lado1==lado2 && lado1==lado3){
-                alert("É um triângulo equilátero :p");
+                document.getElementById("textarea").value="É um triângulo equilátero";
             }
             else if(lado1==lado2 && lado1!=lado3){
-                alert("É um triângulo isósceles :p");
+                document.getElementById("textarea").value="É um triângulo isósceles";
             }
             else if(lado1!=lado2 && lado1!=lado3){
-                alert("É um triângulo escaleno :p");
+                document.getElementById("textarea").value="É um triângulo escaleno";
             }
         }
     }
