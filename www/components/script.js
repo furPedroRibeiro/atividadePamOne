6afdb917e66bb6723799f8582e9f1b145b204eac
furPedroRibeiro <pedrocodejob@gmail.com>
2022-03-15 00:19:45 +0900

    Project's Initial Commit

--- www/components/script.js
+++ www/components/script.js
@@ -1,22 +1,20 @@
-    
-    function MostrarTriangulo(){
-        let lado1=document.getElementById("lado1").value;
-        let lado2=document.getElementById("lado2").value;
-        let lado3=document.getElementById("lado3").value;
+function Write(msg){
+    document.getElementById('textarea').innerHTML = msg;
+}
 
-
-        if(lado1>(lado2+lado3) || lado2>(lado1+lado3) || lado3>(lado1+lado2)){
-            document.getElementById("textarea").value="Não é triângulo";
-        }
-        else{
-            if(lado1==lado2 && lado1==lado3){
-                document.getElementById("textarea").value="É um triângulo equilátero";
+function MostrarTriangulo(){
+        let A =parseInt(document.getElementById("lado1").value);
+        let B =parseInt(document.getElementById("lado2").value);
+        let C =parseInt(document.getElementById("lado3").value);
+        if( (A<B+C) && (B<A+C) && (C<B+A) ){
+            if(A == B && B == C){
+                Write('Equilátero');
+            }else if(A == C || A == B || B == C){
+                Write('Isósceles');
+            }else{
+                Write('Escaleno');
             }
-            else if(lado1==lado2 && lado1!=lado3){
-                document.getElementById("textarea").value="É um triângulo isósceles";
-            }
-            else if(lado1!=lado2 && lado1!=lado3){
-                document.getElementById("textarea").value="É um triângulo escaleno";
-            }
-        }
+    }else{
+        Write('Não é um triângulo');
     }
+}
